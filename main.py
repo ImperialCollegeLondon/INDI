e2b85ab9d0f8a529e60369e5d22bc14080e7ccaa
Alberto Di Biase <asdibiase@uc.cl>
2024-08-27 14:47:51 +0100

    Add rotataion

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ from extensions.initial_setup import initial_setup
 from extensions.read_data.read_and_pre_process_data import read_data
 from extensions.registration_ex_vivo.registration import RegistrationExVivo
 from extensions.segmentation.heart_segmentation import HeartSegmentation
+from extensions.rotation import rotate_data
 from extensions.select_outliers import select_outliers
 from extensions.tensor_fittings import dipy_tensor_fit
 from extensions.u_net_segmentation import get_average_images
@@ -117,6 +118,13 @@ for current_folder in all_to_be_analysed_folders:
     if settings["workflow_mode"] == "reg":
         logger.info("Registration only mode is True. Stopping here.")
         continue
+
+    # =========================================================
+    # Rotation if ex-vivo
+    # =========================================================
+    if settings["ex_vivo"] and settings["rotate"]:
+        logger.info("Ex-vivo rotation is True")
+        data, slices = rotate_data(data, slices, settings, logger)
 
     # =========================================================
     # Remove outliers (pre-segmentation)
