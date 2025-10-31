6808e96bf634cee8ac70ffbaa84c50e1664435e1
Pedro Ferreira <pedro.filipe@outlook.com>
2025-10-31 14:06:48 +0000

    Adds app icon and related scripts
    Adds files for generating the application icon in different sizes
    and a script to run INDI with specified settings via terminal.
    
    Also, moves icon assets to the ui_icons directory.

---
+++ assets/app_icon/convert_to_icon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+convert icon_2.png -resize 16x16 16.png
+convert icon_2.png -resize 24x24 24.png
+convert icon_2.png -resize 32x32 32.png
+convert icon_2.png -resize 48x48 48.png
+convert icon_2.png -resize 64x64 64.png
+convert icon_2.png -resize 128x128 128.png
+convert icon_2.png -resize 256x256 256.png
+convert icon_2.png -resize 512x512 512.png
+convert icon_2.png -resize 1024x1024 1024.png
+convert 256.png Icon.ico	
